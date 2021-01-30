0350d894fb4934bb594df9102b397592cb239318
Sandaruwan <sandaruwan.18@cse.mrt.ac.lk>
2021-01-30 16:38:52 +0530

    add database

--- app/src/main/java/lk/ac/mrt/cse/dbs/simpleexpensemanager/data/impl/PersistentTransactionDAO.java
+++ app/src/main/java/lk/ac/mrt/cse/dbs/simpleexpensemanager/data/impl/PersistentTransactionDAO.java
@@ -1,25 +1,38 @@
 package lk.ac.mrt.cse.dbs.simpleexpensemanager.data.impl;
 
+import android.content.Context;
+
 import java.util.Date;
 import java.util.List;
 
 import lk.ac.mrt.cse.dbs.simpleexpensemanager.data.TransactionDAO;
+import lk.ac.mrt.cse.dbs.simpleexpensemanager.data.db.DbHelper;
 import lk.ac.mrt.cse.dbs.simpleexpensemanager.data.model.ExpenseType;
 import lk.ac.mrt.cse.dbs.simpleexpensemanager.data.model.Transaction;
 
 public class PersistentTransactionDAO implements TransactionDAO {
+    private DbHelper dbHelper;
+
+    public PersistentTransactionDAO(Context context) {
+        this.dbHelper = new DbHelper(context);
+    }
+
     @Override
     public void logTransaction(Date date, String accountNo, ExpenseType expenseType, double amount) {
-        
+        if (accountNo != null) {
+            Transaction transaction = new Transaction(date,accountNo,expenseType,amount);
+            dbHelper.logTransaction(transaction);
+        }
+
     }
 
     @Override
     public List<Transaction> getAllTransactionLogs() {
-        return null;
+        return dbHelper.getTransactions();
     }
 
     @Override
     public List<Transaction> getPaginatedTransactionLogs(int limit) {
-        return null;
+        return dbHelper.getTransactions(limit);
     }
 }
